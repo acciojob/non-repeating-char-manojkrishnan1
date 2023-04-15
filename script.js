81e44ea016fe67189e3aae37670700c64a8e3a27
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-04-15 14:09:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+//your JS code here. If required.
 let input = prompt("Enter a string");
 let n = input.length;
 let count=1;
 let ans = '';
-for(let i=0;i<n-1;i++){
-	let currentCharaceter = input.charAt(i);
-    count = 1;
-    while(currentCharaceter==input.charAt(i+1)){
-        count++;
-        i++;
-    }
-    if(count==1){
-        ans+=currentCharaceter;
-    }
+for(let i=0;i<n;i++){
+		let currentCharaceter = input.charAt(i);
+	    count=1;
+	    for(let j=i+1;j<n;j++){
+	        if(currentCharaceter == input.charAt(j))
+	        count++;
+	    }
+	    if(count==1){
+	    ans+=currentCharaceter;
+	    break;
+	}
 }
-if(ans.length>0)
 alert(ans);
-else
-alert(input.charAt(n-1));
